5a1a51b8c97ba917f5fe8b19761b5d2cd3e4e2dc
nitin-singh-11308 <nitinsingh9963@gmail.com>
2026-04-02 20:47:32 +0530

    add server response dto to calculate system credential and server knowledge

--- HMS/src/main/java/com/myorganization/HMS/dto/response/ServerStatusResponseDto.java
+++ HMS/src/main/java/com/myorganization/HMS/dto/response/ServerStatusResponseDto.java
@@ -1,4 +1,43 @@
 package com.myorganization.HMS.dto.response;
 
+import lombok.Getter;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+@Getter
 public class ServerStatusResponseDto {
+    private final String status;
+    private final String artifact;
+    private final String date;
+    private final String time;
+    private final String timeZoneId;
+    private final String upTime;
+    private final String os;
+
+    public ServerStatusResponseDto (Instant serverStartTime, String applicationName) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        ZoneId zoneId = ZoneId.systemDefault();
+        ZoneOffset zoneOffset = OffsetDateTime.now(zoneId).getOffset();
+
+        this.status = "Hospital Server is Live";
+        this.artifact = applicationName;
+        this.date = currentDateTime.format(dateFormatter);
+        this.time = currentDateTime.format(timeFormatter);
+        this.timeZoneId = String.format("%s (UTC%s)", zoneId.getId(), zoneOffset);
+        this.upTime = calculateUptime(serverStartTime);
+        this.os = String.format("%s (%s)",System.getProperty("os.name"),System.getProperty("os.version"));
+    }
+
+    private String calculateUptime(Instant serverStartTime) {
+        Duration duration = Duration.between(serverStartTime, Instant.now());
+        long hours = duration.toHours();
+        long minutes = duration.toMinutes();
+        long seconds = duration.getSeconds();
+
+        return String.format("%02d hours %02d minutes %02d seconds", hours, minutes, seconds);
+    }
 }
