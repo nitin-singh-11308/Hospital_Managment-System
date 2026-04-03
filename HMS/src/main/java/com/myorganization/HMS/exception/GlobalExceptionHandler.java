a9d9353b39635abbb1d585b9e38cf5e2db3bea8f
nitin-singh-11308 <nitinsingh9963@gmail.com>
2026-04-03 22:52:38 +0530

    Create custom 'GlobalExceptionHandler' to handle all exceptions

--- HMS/src/main/java/com/myorganization/HMS/exception/GlobalExceptionHandler.java
+++ HMS/src/main/java/com/myorganization/HMS/exception/GlobalExceptionHandler.java
@@ -1,7 +1,22 @@
 package com.myorganization.HMS.exception;
 
+import com.myorganization.HMS.dto.response.GenericResponseDto;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException {
-  public GlobalExceptionHandler(String message) {
-    super(message);
-  }
+    @ExceptionHandler(DoctorNotFoundException.class)
+    public ResponseEntity<GenericResponseDto> handleDoctorNotFoundException(DoctorNotFoundException e) {
+        GenericResponseDto genericResponseDto = new GenericResponseDto();
+
+        genericResponseDto.setSuccess(false);
+        genericResponseDto.setMessage(e.getMessage());
+        genericResponseDto.setDetails(null);
+
+        return new ResponseEntity<>(genericResponseDto, HttpStatusCode.valueOf(404));
+    }
+
 }
